72d42e472ee9288e91ca9fe29d2077216ef4b0fa
kazuoikeda1411 <erizabesu1411@icloud.com>
2022-12-17 18:03:40 +0900

    ディレクトリ内ファイルを整理、Mapperファイル名を変更

--- src/main/java/com/raisetech/lecture09/Disease.java
+++
@@ -1,4 +0,0 @@
-package com.raisetech.lecture09;
-
-public record Disease(String name, String disease) {
-}
--- src/main/java/com/raisetech/lecture09/MedicalRecord.java
+++
@@ -1,4 +0,0 @@
-package com.raisetech.lecture09;
-
-public record MedicalRecord(int id, String name, String disease) {
-}
